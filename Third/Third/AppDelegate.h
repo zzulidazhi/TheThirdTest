84f24ddb38b85d56c327cf796844a69ea869412b
zhangyongzhi <zzulidazhi@gmail.com>
2015-08-20 17:42:21 +0800

    hehe

---
+++ Third/Third/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Third
+//
+//  Created by 张永治 on 15/8/20.
+//  Copyright (c) 2015年 张永治. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Third/Third/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Third
+//
+//  Created by 张永治 on 15/8/20.
+//  Copyright (c) 2015年 张永治. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
